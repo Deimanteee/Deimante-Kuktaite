676f1cc34ef40e059194767935de894e723bafb1
Deimanteee <deima.kuktaite@gmail.com>
2019-03-21 16:54:46 +0200

    Update main.cpp

--- 0.1v/main.cpp
+++ 0.1v/main.cpp
@@ -1,14 +1,35 @@
 #include <iostream>
+#include <vector>
+
 
 using namespace std;
+struct eilute{
+    string vardas, pavarde;
+    double egz, vid, med, rezultatas;
+    vector<float>paz;
+};
 
 int main()
-{   char a;
+{   char a; double j, sum=0;
     while (true){
     cout << "Hello world!" << endl;
     cout<<123<<endl;
-    cin>>a;
-    if (a=='t') break;
+
+    eilute k1;
+    k1.vardas="ggffdf";
+    k1.pavarde="erwerewrw";
+    k1.egz=8;
+    for (int i=0; i<5; i++){
+            cout<<"Iveskite paz.: ";
+            cin>>j;
+        k1.paz.push_back(j);
+        sum=sum+j;
+    }
+    k1.vid=sum/k1.paz.size();
+    k1.rezultatas=0.3*k1.vid+0.7*k1.egz;
+    cout<<k1.vardas<<"; "<<k1.pavarde<<"; "<< k1.egz<<"; "<<k1.vid<<"; "<<k1.rezultatas<<endl;
+        cin>>a;
+        if (a=='t') break;
     }
     return 0;
 }
