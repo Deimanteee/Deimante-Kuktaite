89a450864f0dd90ee8830440bb1819adcad46504
Deimanteee <deima.kuktaite@gmail.com>
2019-03-21 16:37:38 +0200

    dasd

--- 0.1v/main.cpp
+++ 0.1v/main.cpp
@@ -3,8 +3,12 @@
 using namespace std;
 
 int main()
-{
+{   char a;
+    while (true){
     cout << "Hello world!" << endl;
-    cout<<123;
+    cout<<123<<endl;
+    cin>>a;
+    if (a=='t') break;
+    }
     return 0;
 }
