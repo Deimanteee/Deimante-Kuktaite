de6408bee4f60ed06ddfe8515dbe5ebf8af0158c
Deimanteee <deima.kuktaite@gmail.com>
2019-04-23 18:29:42 +0300

    Trecias

---
+++ 0.1v/Trecias/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <conio.h>
+#include <string>
+#include <iomanip>
+#include <vector>
+
+using namespace std;
+
+struct studentas
+{
+    string vardas;
+    string pavarde;
+    double vidurkis = 0;
+    double med = 0;
+    int egz = 0;
+};
+
+int main()
+{
+
+
+    return 0;
+}
