4ebb352181d86fc9291af2f3181e53231616c05e
Deimanteee <48588345+Deimanteee@users.noreply.github.com>
2019-04-27 22:16:58 +0300

    Add files via upload

---
+++ Labaratorinis.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+struct Studentas
+{
+    string vardas;
+    string pavarde;
+    double vidurkis = 0;
+    double median = 0;
+    double galutinis = 0;
+    double mediana = 0;
+    int egzaminas;
+    vector<int>pazimys;
+
+    }
